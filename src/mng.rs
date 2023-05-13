425b86863ed98c0c2ca0c20fc7eb12362b4bea03
Guillaume PELLEGRINO <guillaume.pellegrino@yahoo.com>
2023-05-13 12:43:12 +0200

    scrap old management APIs

--- src/mng.rs
+++ src/mng.rs
@@ -23,9 +23,6 @@ use bytes::Bytes;
 use eyre::{eyre, Result, WrapErr};
 use http_body_util::Full;
 use hyper::{body::Incoming as IncomingBody, Request, Response};
-use regex::Regex;
-use serde::Deserialize;
-use serde::Serialize;
 use std::collections::hash_map::Entry::{Occupied, Vacant};
 use std::collections::HashMap;
 use std::sync::Arc;
@@ -144,217 +141,6 @@ impl ManagementSession {
                 api::Response::error(502, &format!("Failed to transfer request to CPE: {:?}", e))
             }
         }
-    }
-
-    async fn soap_response(soap_result: &Result<soap::Envelope>) -> Result<Response<Full<Bytes>>> {
-        match soap_result {
-            Ok(envelope) => {
-                let s = format!("> {:?}:\n{}\n", envelope.kind(), envelope);
-                utils::reply(200, s)
-            }
-            Err(err) => {
-                let s = format!("> Error:\n{:?}\n", err);
-                utils::reply(400, s)
-            }
-        }
-    }
-
-    async fn handle_gpn_request(
-        &mut self,
-        serial_number: &str,
-        content: &str,
-    ) -> Result<Response<Full<Bytes>>> {
-        let mut envelope = soap::Envelope::new("");
-        envelope.add_gpn(content, true);
-        let result = self.cpe_transfer(serial_number, envelope).await;
-        Self::soap_response(&result).await
-    }
-
-    async fn handle_gpv_request(
-        &mut self,
-        serial_number: &str,
-        content: &str,
-    ) -> Result<Response<Full<Bytes>>> {
-        let mut envelope = soap::Envelope::new("");
-        let gpv = envelope.add_gpv();
-        for param in content.split(';') {
-            gpv.push(param);
-        }
-        let result = self.cpe_transfer(serial_number, envelope).await;
-        Self::soap_response(&result).await
-    }
-
-    async fn handle_spv_request(
-        &mut self,
-        serial_number: &str,
-        content: &str,
-    ) -> Result<Response<Full<Bytes>>> {
-        let mut gpv_envelope = soap::Envelope::new("");
-        let mut spv_envelope = soap::Envelope::new("");
-        let gpv = gpv_envelope.add_gpv();
-        let spv = spv_envelope.add_spv(1);
-        let regex_key_type_value = Regex::new(r"(.+)<(.+)>=(.+)")?;
-        let regex_key_value = Regex::new(r"(.+)=(.+)")?;
-        for param in content.split(';') {
-            match regex_key_type_value.captures(param) {
-                Some(captures) => {
-                    let key = captures.get(1).ok_or(eyre!("invalid expression"))?.as_str();
-                    let base_type = captures.get(2).ok_or(eyre!("invalid expression"))?.as_str();
-                    let xsd_type = format!("xsd:{}", base_type);
-                    let value = captures.get(3).ok_or(eyre!("invalid expression"))?.as_str();
-                    //println!("SPV({},{},{})", key, xsd_type, value);
-                    spv.push(soap::ParameterValue::new(key, &xsd_type, value));
-                }
-                None => {
-                    // let's query the parameter type with a GPV
-                    let captures = regex_key_value
-                        .captures(param)
-                        .ok_or(eyre!("invalid expression"))?;
-
-                    let key = captures.get(1).ok_or(eyre!("invalid expression"))?.as_str();
-                    let value = captures.get(2).ok_or(eyre!("invalid expression"))?.as_str();
-                    gpv.push(key);
-                    spv.push(soap::ParameterValue::new(key, "", value));
-                }
-            };
-        }
-
-        // Send a GPV to deduct the parameter types
-        if gpv.len() > 0 {
-            let result = self.cpe_transfer(serial_number, gpv_envelope).await?;
-            let response = match result.body.gpv_response.first() {
-                Some(response) => response,
-                None => {
-                    return Self::soap_response(&Ok(result)).await;
-                }
-            };
-            for gpv_pv in &response.parameter_list.parameter_values {
-                for spv_pv in &mut spv.parameter_list.parameter_values {
-                    if gpv_pv.name != spv_pv.name {
-                        continue;
-                    }
-                    spv_pv.value.xsi_type = gpv_pv.value.xsi_type.clone();
-                }
-            }
-        }
-
-        let result = self.cpe_transfer(serial_number, spv_envelope).await;
-        Self::soap_response(&result).await
-    }
-
-    async fn handle_download_request(
-        &mut self,
-        serial_number: &str,
-        content: &str,
-    ) -> Result<Response<Full<Bytes>>> {
-        #[derive(Debug, PartialEq, Default, Deserialize, Serialize)]
-        struct Download {
-            #[serde(default)]
-            command_key: String,
-
-            #[serde(default)]
-            file_type: String,
-
-            #[serde(default)]
-            url: String,
-
-            #[serde(default)]
-            username: String,
-
-            #[serde(default)]
-            password: String,
-
-            #[serde(default)]
-            file_size: i64,
-
-            #[serde(default)]
-            target_file_name: String,
-
-            #[serde(default)]
-            delay_seconds: i32,
-
-            #[serde(default)]
-            success_url: String,
-
-            #[serde(default)]
-            failure_url: String,
-        }
-        let download: Download = serde_qs::from_str(content)?;
-
-        let mut envelope = soap::Envelope::new("");
-        envelope
-            .add_download()
-            .set_command_key(&download.command_key)
-            .set_file_type(&download.file_type)
-            .set_url(&download.url)
-            .set_username(&download.username)
-            .set_password(&download.password)
-            .set_file_size(download.file_size)
-            .set_target_file_name(&download.target_file_name)
-            .set_delay_seconds(download.delay_seconds)
-            .set_success_url(&download.success_url)
-            .set_failure_url(&download.failure_url);
-
-        let result = self.cpe_transfer(serial_number, envelope).await;
-        Self::soap_response(&result).await
-    }
-
-    async fn handle_upgrade_request(
-        &mut self,
-        serial_number: &str,
-        content: &str,
-    ) -> Result<Response<Full<Bytes>>> {
-        #[derive(Debug, PartialEq, Default, Deserialize, Serialize)]
-        struct Upgrade {
-            #[serde(default)]
-            file_name: String,
-        }
-        let upgrade: Upgrade = serde_qs::from_str(content)?;
-
-        let acs = self.acs.read().await;
-        let url = format!("${{baseurl}}/download/{}", upgrade.file_name);
-        let mut envelope = soap::Envelope::new("");
-        envelope
-            .add_download()
-            .set_command_key("upgrade")
-            .set_file_type("1 Firmware Upgrade Image")
-            .set_url(&url)
-            .set_username(&acs.config.username)
-            .set_password(&acs.config.password)
-            .set_target_file_name(&upgrade.file_name);
-        drop(acs);
-
-        let result = self.cpe_transfer(serial_number, envelope).await;
-        Self::soap_response(&result).await
-    }
-
-    async fn handle_list_request(&self) -> Result<Response<Full<Bytes>>> {
-        let acs = self.acs.read().await;
-        let mut s = format!("{}x Managed CPEs:\n", acs.cpe_list.len());
-
-        for (sn, cpe) in &acs.cpe_list {
-            let cpe = cpe.read().await;
-            s += &format!(
-                "{} - {} - {} - {} \n",
-                sn, cpe.connreq.url, cpe.connreq.username, cpe.connreq.password
-            );
-        }
-        utils::reply(200, s)
-    }
-
-    async fn handle_snlist_request(&self) -> Result<Response<Full<Bytes>>> {
-        let acs = self.acs.read().await;
-        let mut s = String::new();
-
-        for sn in acs.cpe_list.keys() {
-            s += &format!("{}\n", sn);
-        }
-        utils::reply(200, s)
-    }
-
-    async fn handle_stats_request(&self) -> Result<Response<Full<Bytes>>> {
-        let s = "Stats not implemented".to_string();
-        utils::reply(200, s)
     }
 
     async fn get_parameter_names(
@@ -519,26 +305,13 @@ impl ManagementSession {
         utils::reply(404, s)
     }
 
-    // TODO:
-    // - Check if a session is already opened before sending a ConnectionRequest.
-    // - Maintain the connection to CPE open as long as one management session is opened.
-    //
     pub async fn handle(
         &mut self,
         req: &mut Request<IncomingBody>,
     ) -> Result<Response<Full<Bytes>>> {
         let command = utils::req_path(req, 1);
-        let serial_number = utils::req_path(req, 2);
         let content = utils::content(req).await?;
         let reply = match command.as_str() {
-            "gpn" => self.handle_gpn_request(&serial_number, &content).await,
-            "gpv" => self.handle_gpv_request(&serial_number, &content).await,
-            "spv" => self.handle_spv_request(&serial_number, &content).await,
-            "download" => self.handle_download_request(&serial_number, &content).await,
-            "upgrade" => self.handle_upgrade_request(&serial_number, &content).await,
-            "list" => self.handle_list_request().await,
-            "snlist" => self.handle_snlist_request().await,
-            "stats" => self.handle_stats_request().await,
             "api" => self.handle_api_request(&content).await,
             "" => self.handle_welcome_request().await,
             _ => self.handle_err404(req, &content).await,
